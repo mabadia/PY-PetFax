645468b4fc78ce24988cac6d1209b188df2248da
mabadia <angel.beyou95@gmail.com>
2023-11-08 21:32:19 -0800

    part 3

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -6,5 +6,8 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
-    
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+    # return the app
     return app
